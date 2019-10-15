fdf153ab68baf081f13017019ae077919b8608a0
Karen Ventura <akv802@gmail.com>
2019-10-15 14:20:19 -0500

    [RabiEZTx] - Add single quotes and ignore files

--- app/javascript/packs/steps.js
+++ app/javascript/packs/steps.js
@@ -1,12 +1,12 @@
 /*jshint esversion: 6 */
-"use strict";
+'use strict';
 
 window.Egf = window.Egf || {};
 Egf.StepTabs = function() {};
 
 Egf.StepTabs.prototype = {
   init: function() {
-    let tabsContainers = document.querySelectorAll(".steps__container");
+    let tabsContainers = document.querySelectorAll('.steps__container');
     let _this = this;
     tabsContainers.forEach(function(tabsContainer) {
       _this.initTabs(tabsContainer);
@@ -14,24 +14,24 @@ Egf.StepTabs.prototype = {
   },
 
   initTabs: function(tabsContainer) {
-    let tabs = tabsContainer.querySelectorAll(".levels .steps__item");
-    let tabsContent = tabsContainer.querySelectorAll(".step__content");
+    let tabs = tabsContainer.querySelectorAll('.levels .steps__item');
+    let tabsContent = tabsContainer.querySelectorAll('.step__content');
 
     let deactvateAllTabs = function () {
       tabs.forEach(function (tab) {
-        let tabMarker = tab.children.item("steps__marker");
-        tabMarker.classList.remove("steps__marker--active");
+        let tabMarker = tab.children.item('steps__marker');
+        tabMarker.classList.remove('steps__marker--active');
       });
     };
 
     let hideTabsContent = function () {
       tabsContent.forEach(function (tabContent) {
-        tabContent.classList.remove("is-active");
+        tabContent.classList.remove('is-active');
       });
     };
 
     let activateTabsContent = function (tab) {
-      tabsContent[getIndex(tab)].classList.add("is-active");
+      tabsContent[getIndex(tab)].classList.add('is-active');
     };
 
     let getIndex = function (el) {
@@ -39,11 +39,11 @@ Egf.StepTabs.prototype = {
     };
 
     tabs.forEach(function (tab) {
-      tab.addEventListener("click", function () {
+      tab.addEventListener('click', function () {
         deactvateAllTabs();
         hideTabsContent();
-        let tabMarker = tab.children.item("steps__marker");
-        tabMarker.classList.add("steps__marker--active");
+        let tabMarker = tab.children.item('steps__marker');
+        tabMarker.classList.add('steps__marker--active');
         activateTabsContent(tab);
       });
     });
--- app/javascript/packs/tabs.js
+++ app/javascript/packs/tabs.js
@@ -1,5 +1,5 @@
 /*jshint esversion: 6 */
-"use strict";
+'use strict';
 
 window.Egf = window.Egf || {};
 Egf.BulmaTabs = function() {};
@@ -10,23 +10,23 @@ Egf.BulmaTabs.prototype = {
     },
 
     initTabs: function() {
-        let tabs = document.querySelectorAll(".tabs li");
-        let tabsContent = document.querySelectorAll(".tab__content");
+        let tabs = document.querySelectorAll('.tabs li');
+        let tabsContent = document.querySelectorAll('.tab__content');
 
         let deactvateAllTabs = function () {
           tabs.forEach(function (tab) {
-            tab.classList.remove("is-active");
+            tab.classList.remove('is-active');
           });
         };
 
         let hideTabsContent = function () {
           tabsContent.forEach(function (tabContent) {
-            tabContent.classList.remove("is-active");
+            tabContent.classList.remove('is-active');
           });
         };
 
         let activateTabsContent = function (tab) {
-          tabsContent[getIndex(tab)].classList.add("is-active");
+          tabsContent[getIndex(tab)].classList.add('is-active');
         };
 
         let getIndex = function (el) {
@@ -35,15 +35,15 @@ Egf.BulmaTabs.prototype = {
 
         let resetStepTabs = function(tab) {
           let tabContent = tabsContent[getIndex(tab)]
-          let tabsContainer = tabContent.querySelector(".steps__container");
+          let tabsContainer = tabContent.querySelector('.steps__container');
           new Egf.StepTabs().initTabs(tabsContainer);
         };
 
         tabs.forEach(function (tab) {
-          tab.addEventListener("click", function () {
+          tab.addEventListener('click', function () {
             deactvateAllTabs();
             hideTabsContent();
-            tab.classList.add("is-active");
+            tab.classList.add('is-active');
             activateTabsContent(tab);
             resetStepTabs(tab);
           });
